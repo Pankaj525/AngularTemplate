a0bdf4629be719ab289774d5b557cc6dc011bbb2
DESKTOP-NSL6CS6\Pankaj <pankaj.madisetti@gmail.com>
2018-11-04 18:26:53 +0530

    removed unnecessary menus

--- AngularWithSCSS/src/app/admin/dashboard/dashboard-routing.module.ts
+++ AngularWithSCSS/src/app/admin/dashboard/dashboard-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: DashboardComponent,canActivate: [RoleGuardGuard], 
     data: {
       title: 'Dashboard',
-      expectedRole: 'User'
+      expectedRole: 'Admin'
     }
   }
 ];
